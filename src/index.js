0d5c225656318d9f798bbe92e418f9466e827de7
Jovana <jovana.usorac1995@gmail.com>
2019-12-10 00:52:27 +0100

    first react homework

--- src/index.js
+++ src/index.js
@@ -4,9 +4,32 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+  }
+  
+  const user = {
+    firstName: 'Jovana',
+    lastName: 'Usorac',
+  };
+  
+  const element = <h1>Hello my name is {formatName(user)}!</h1>;
+  
+  function myFunction(user){
+    return user.lastName;
+  }
+  const element2 = <h1> Trying out my function for last name only : {myFunction(user)} ;
+  Trying out two functions : 
+  Function formatName : {formatName(user)}
+  Function lastName : {lastName(user)}</h1>
+  ReactDOM.render(element2, document.getElementById('root'));
+  
+  ReactDOM.render(element2, document.getElementById('root'));
+  
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// I dont understand why 2 functions between <h1></h1> dont work, it only works with one
+/*I also dont get why I can only get element or element2 written out, if I put them together
+in ReactDOM. it wont work*/
 serviceWorker.unregister();
